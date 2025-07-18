61f691ff1358713753f00f1b769ba10183cc4c2a
ShweatyWarker <nicholas.gibson@triosstudent.com>
2025-07-18 15:48:56 -0400

    I answered it, but not sure if I added it right

---
+++ Desktop/JavaAssignment1/Exercise2.7.java/Answers2.7.java
@@ -0,0 +1,13 @@
+public class Answers2.7 {
+  public class Chapter2_7Answers {
+    public static void main(String[] args) {
+        // Chapter 2.7 – Fill in the blanks
+
+        // 1. Comments are used to document a program and improve its readability.
+        // 2. A decision can be made in a Java program with an if statement.
+        // 3. The arithmetic operators with the same precedence as multiplication are division and modulus.
+        // 4. When parentheses in an arithmetic expression are nested, the innermost set of parentheses is evaluated first.
+        // 5. A location in the computer’s memory that may contain different values at various times during the execution of a program is called a variable.
+    }
+}  
+}
